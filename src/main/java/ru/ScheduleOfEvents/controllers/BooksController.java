3a74087e4c6a07f2809b69995beb98f9c4514f41
DmitriyPetunin <154313405+DmitriyPetunin@users.noreply.github.com>
2024-04-06 21:19:34 +0300

    add base html with navbar and footer

--- src/main/java/ru/ScheduleOfEvents/controllers/BooksController.java
+++ src/main/java/ru/ScheduleOfEvents/controllers/BooksController.java
@@ -1,9 +1,18 @@
 package ru.ScheduleOfEvents.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@RequestMapping("/book")
+@RequestMapping()
 public class BooksController {
+    @GetMapping
+    public String test(){
+        return "base.html";
+    }
+    @GetMapping("/events")
+    public String test2(){
+        return "Events.html";
+    }
 }
