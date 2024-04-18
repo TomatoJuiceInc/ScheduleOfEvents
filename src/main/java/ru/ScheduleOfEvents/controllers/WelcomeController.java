a83fb97e2f220971fd0c9092b7c8c668d4116bb1
khairullovruslan <khairullov.ruslan@gmail.com>
2024-04-18 22:09:29 +0300

    fix

--- src/main/java/ru/ScheduleOfEvents/controllers/WelcomeController.java
+++ src/main/java/ru/ScheduleOfEvents/controllers/WelcomeController.java
@@ -3,6 +3,7 @@ package ru.ScheduleOfEvents.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -11,5 +12,9 @@ public class WelcomeController {
     @GetMapping()
     public String showPage(){
         return "welcome/welcome";
+    }
+    @PostMapping("welcome")
+    public String redirectForEventPage(){
+        return "redirect:/";
     }
 }
