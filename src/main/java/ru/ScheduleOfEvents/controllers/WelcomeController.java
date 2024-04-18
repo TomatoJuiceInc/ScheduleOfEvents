81e52135dcb426acf390040225ba4d31b553f722
khairullovruslan <khairullov.ruslan@gmail.com>
2024-04-18 21:32:37 +0300

    create welcome page

---
+++ src/main/java/ru/ScheduleOfEvents/controllers/WelcomeController.java
@@ -0,0 +1,15 @@
+package ru.ScheduleOfEvents.controllers;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping()
+public class WelcomeController {
+    @GetMapping()
+    public String showPage(){
+        return "welcome/welcome";
+    }
+}
