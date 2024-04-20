cc0064fccb0458024cfd979cf34de0a64180ce08
khairullovruslan <khairullov.ruslan@gmail.com>
2024-04-20 18:35:14 +0300

    add errorpage

--- src/main/java/ru/ScheduleOfEvents/controllers/CustomErrorController.java
+++ src/main/java/ru/ScheduleOfEvents/controllers/CustomErrorController.java
@@ -1,13 +1,13 @@
 package ru.ScheduleOfEvents.controllers;
 
+import org.springframework.boot.web.servlet.error.ErrorController;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@RequestMapping("/error1")
-public class ErrorController {
-    @GetMapping()
+public class CustomErrorController implements ErrorController {
+    @RequestMapping("/error")
     public String errorPage(){
         System.out.println("Sdf");
         return "error/error";
