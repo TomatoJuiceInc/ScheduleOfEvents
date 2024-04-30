897c8af3e96cdf8bbce5d3a7cde19e0b5baf5047
Kirill Akhmetov <kirillahmetov935@gmail.com>
2024-04-30 23:22:30 +0300

    Fixed problem with an admin account

--- src/main/java/ru/ScheduleOfEvents/services/AdminService.java
+++ src/main/java/ru/ScheduleOfEvents/services/AdminService.java
@@ -7,22 +7,23 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import ru.ScheduleOfEvents.models.User;
 import ru.ScheduleOfEvents.models.UserRole;
-import ru.ScheduleOfEvents.repositories.UserRepository;
 
 @Service
 //@RequiredArgsConstructor
 @AllArgsConstructor
 public class AdminService {
-    private final UserRepository userRepository;
     private final PasswordEncoder passwordEncoder;
+    private final UserDetailsServiceImpl userDetailsService;
 
-//    @PostConstruct
-//    @Transactional
-//    public void postConstruct() {
-//        User admin = new User();
-//        admin.setUsername("admin");
-//        admin.setPassword(passwordEncoder.encode("admin"));
-//        admin.setRole(UserRole.ADMIN.name());
-//        userRepository.save(admin);
-//    }
+    @PostConstruct
+    @Transactional
+    public void postConstruct() {
+        if (userDetailsService.findByUsername("admin") == null) {
+            User admin = new User();
+            admin.setUsername("admin");
+            admin.setPassword(passwordEncoder.encode("admin"));
+            admin.setRole(UserRole.ADMIN.name());
+            userDetailsService.save(admin);
+        }
+    }
 }
